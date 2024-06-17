2909d1e7fd752e5f8cf808c730184ee6bf829527
Miguigg <miguigg1412@gmail.com>
2024-06-17 13:41:03 +0200

    Creación del proyecto

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0cs-pFgNfuZ3WBB1-y-JWgIFO7HtabNk",
+  authDomain: "dossier-f6634.firebaseapp.com",
+  projectId: "dossier-f6634",
+  storageBucket: "dossier-f6634.appspot.com",
+  messagingSenderId: "372146943992",
+  appId: "1:372146943992:web:4a95bf8bcb7087fdce880d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
